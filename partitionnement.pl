857c1b16f7233e4501d33a2126631201aedb32cb
Raphael Jacob-Simard <RaphaelJ-S@hotmail.com>
2021-11-23 08:59:37 -0500

    ajout des fichiers de départ

---
+++ partitionnement.pl
@@ -0,0 +1,29 @@
+
+/**
+ * Calcul la distance entre deux phrases.
+ * Cette valeur est un point flottant.
+ * distance( +PhraseA, +PhraseB, -Distance ).
+ * les arguments PhraseA et PhraseB sont des listes de mots (atome).
+ * Si les phrases sont trop différente, alors la différence est de 1000.0.
+ */
+distance( PhraseA, PhraseB, Distance ) :-
+	intersection( PhraseA, PhraseB, MotsCommuns ),
+	length( PhraseA, NbrMotA ),
+	length( PhraseB, NbrMotB ),
+	MaxCommun is min( NbrMotA, NbrMotB ),
+	length( MotsCommuns, NbrMotsCommuns ),
+	( ( \+ NbrMotsCommuns =:= 0.0,
+	    Distance is MaxCommun / NbrMotsCommuns );
+	  ( NbrMotsCommuns =:= 0.0,
+	    Distance is 1000.0 )
+	).
+
+/**
+ * Prédicat principal.
+ * Ce prédicat charge la base de connaissance contenant le texte à résumer.
+ * Ensuite il construit le résumé et l'affiche.
+ */
+resoudre( Nom, K ) :-
+	consult( Nom ),
+	% vos appels de prédicats ici pour le reste du programme.
+    .
---
+++ texte1.pl
@@ -0,0 +1,11 @@
+
+p( s1, ['pour', 'construire', 'les', 'partitions', 'd', 'un', 'texte', 'vous', 
+               'devez', 'construire', 'un', 'graphe', 'representant', 'les', 
+			   'donnees'] ).
+p( s2, ['chaque', 'noeud', 'du', 'graphe', 'represente', 'une', 'partition'] ).
+p( s3, ['une', 'partition', 'est', 'un', 'ensemble', 'de', 'phrases'] ).
+p( s4, ['initialement', 'chaque', 'partition', 'contient', 'une', 'seule', 
+               'phrase'] ).
+p( s5, ['il', 'y', 'aura', 'donc', 'autant', 'de', 'partitions', 'qu', 'il', 'y', 
+               'a', 'de', 'phrase'] ).
+p( s6, ['un', 'arc', 'est', 'place', 'entre', 'chaque', 'partition'] ).
