aa3fdb1100a83a09203f933855b277e981e2c571
Raphael Jacob-Simard <RaphaelJ-S@hotmail.com>
2021-12-02 15:50:10 -0500

    finission des commantaire

--- partitionnement.pl
+++ partitionnement.pl
@@ -1,6 +1,6 @@
 % "E:/Depot_Git/inf6120/inf6120-tp2/".
 
-:-dynamic sommet/4, arc/3, compteur/1, cible/1, addition/1, rep/2.
+:-dynamic sommet/3, arc/3, compteur/1, cible/1, addition/1, rep/2.
 
 /**
  * Calcul la distance entre deux phrases.
@@ -83,7 +83,7 @@ initAll(Cible) :-
  * @Sommets - Une liste contenant des éléments sommet/3.
  */
 getSommets(Sommets) :-
-	findall(sommet(A, B, C, D), sommet(A, B, C, D), Sommets).
+	findall(sommet(A, B, C), sommet(A, B, C), Sommets).
 
 /**
  * getArcs(-Arcs)
@@ -98,7 +98,7 @@ getArcs(Arcs) :-
  * Initialise tous les sommets de départ à partir des structures p/2. 
  */
 initSommets :-
-	findall(sommet(Id,[Phrase], Phrase, 0.0), p(Id, Phrase), Sommets),
+	findall(sommet(Id,[Phrase], rep(Phrase, 0.0)), p(Id, Phrase), Sommets),
 	forall(member(Sommet, Sommets), assertz(Sommet)). 
 
 /**
@@ -115,19 +115,31 @@ initArcs :-
 	).
 
 /**
+ * ajouterArcs(+Sommet, +ListeSommets)
+ * Ajoute des arc/3 entre @Sommet et Chaque membre de @ListeSommets.
  * 
+ * @Sommet - Une structure sommet/3
+ * @ListeSommets - Une liste de structures sommet/3
  */
 ajouterArcs(Sommet, ListeSommets) :-
 	forall(
 		member(Bout, ListeSommets),
 		(
-			sommet(IdS,_, RepA, _) = Sommet,
-			sommet(IdB,_, RepB, _) = Bout,
+			sommet(IdA,_, rep(RepA, _)) = Sommet,
+			sommet(IdB,_, rep(RepB, _)) = Bout,
 			distance(RepA, RepB, Distance),
-			assertz(arc(IdS, IdB, Distance))
+			assertz(arc(IdA, IdB, Distance))
 		)
 	).
 
+
+/**
+ * phraseType(+ListePhrase, -PhraseType)
+ * Retourne une structure rep/2, phrase type d'un sommet.
+ *
+ * @ListePhrase - Une liste de phrases.
+ * @PhraseType - La phrase type de @ListePhrase, rep(Phrase, Moyenne)
+ */
 phraseType(ListePhrases, PhraseType) :-
 	length(ListePhrases, X),
 	NbrPhrases is X - 1,
@@ -146,6 +158,14 @@ phraseType(ListePhrases, PhraseType) :-
 	retract(rep(PhraseMin, MoyMin)),
 	PhraseType = rep(PhraseMin, MoyMin).
 
+/**
+ * determineMin(+rep(P1,M1), +rep(P2,M2), -RepMin)
+ * Retourne la représentante ayant la moyenne la plus petite.
+ *
+ * @rep(P1, M1) - La première représentante.
+ * @rep(P2, M2) - La deuxième représentante.
+ * @RepMin - La plus petites des deux représentantes.
+ */
 determineMin(rep(P1, M1), rep(P2, M2), RepMin) :-
 	M1 =< M2,
 	RepMin = rep(P1, M1).
@@ -153,6 +173,15 @@ determineMin(rep(P1, M1), rep(P2, M2), RepMin) :-
 	M2 < M1,
 	RepMin = rep(P2, M2).
 
+/**
+ * moyennePhrase(+Phrase, +Reste, +NbrPhrases, -Moy)
+ * Retourne la moyenne de @Phrase par rapport aux phrase de @Reste
+ *
+ * @Phrase - La phrase pour laquelle on veut définir la moyenne.
+ * @Reste - Une liste de phrase.
+ * @NbrPhrases - Le nombre de phrases dans @Reste
+ * @Moy - La moyenne de @Phrase
+ */
 moyennePhrase(Phrase, Reste, NbrPhrases, Moy) :-
 	asserta(addition(0)),
 	forall(
@@ -162,6 +191,15 @@ moyennePhrase(Phrase, Reste, NbrPhrases, Moy) :-
 	retract(addition(A)),
 	Moy is A / NbrPhrases.
 
+/**
+ * Prédicat intermédiaire
+ * addition(+Phrase, +Autre)
+ * Met à jour l'addition des distances entre deux phrases
+ * dans une structure addition/1 dans la base de connaissance.
+ * 
+ * @Phrase - Une première phrase.
+ * @Autre - Une deuxième phrase. 
+ */
 addition(Phrase, Autre) :-
 	distance(Phrase, Autre, Distance),
 	retract(addition(A)),
@@ -170,11 +208,13 @@ addition(Phrase, Autre) :-
 
 
 
-
+/**
+ * Efface tous les prédicats de la base de connaissance.
+ */
 teardownAll :-
 	abolish(cible/1),
 	abolish(compteur/1),
-	abolish(sommet/4),
+	abolish(sommet/3),
 	abolish(arc/3).
 
 
