51e0e3ee7be1c64ae2f4cf028dd059f49257c7b0
Raphael Jacob-Simard <RaphaelJ-S@hotmail.com>
2021-12-03 14:22:36 -0500

    tout est terminé mais les résultats ne sont pas correct. problème dans le calcul de la distance?

--- partitionnement.pl
+++ partitionnement.pl
@@ -1,31 +1,22 @@
 % "E:/Depot_Git/inf6120/inf6120-tp2/".
-
-:-dynamic sommet/3, arc/3, compteur/1, cible/1, addition/1, rep/2.
-
 /**
- * Calcul la distance entre deux phrases.
- * Cette valeur est un point flottant.
- * distance( +PhraseA, +PhraseB, -Distance ).
- * les arguments PhraseA et PhraseB sont des listes de mots (atome).
- * Si les phrases sont trop différente, alors la différence est de 1000.0.
+ * Nom : Jacob-Simard
+ * Prénom : Raphaël
+ * Code permanent : JACR26038907
+ * Cours : INF6120
+ * Groupe : 20
  */
 
-distance( PhraseA, PhraseB, Distance ) :-
-    distanceD( PhraseA, PhraseB, DistanceAB ),
-    distanceD( PhraseB, PhraseA, DistanceBA ),
-    Distance is ( DistanceAB + DistanceBA ) / 2.0.
+/**
+ * sommet/3 : sommet(Identifiant, Liste de phrases, rep/2)
+ * arc/3 : arc(Identifiant du premier sommet, Identifiant du deuxième sommet, Distance entre les deux sommets)
+ * cible/1 : cible(Le nombre de sommets à atteindre)
+ * addition/1 : addition(valeur temporaire)
+ * rep/2 : rep(Phrase type du sommet, Moyenne)
+ */
+:-dynamic sommet/3, arc/3, cible/1, addition/1, rep/2.
 
-distanceD( PhraseA, PhraseB, Distance ) :-
-    intersection( PhraseA, PhraseB, MotsCommuns ),
-    length( PhraseA, NbrMotA ),
-    length( PhraseB, NbrMotB ),
-    MaxCommun is min( NbrMotA, NbrMotB ),
-    length( MotsCommuns, NbrMotsCommuns ),
-    ( ( \+ NbrMotsCommuns =:= 0.0,
-        Distance is MaxCommun / NbrMotsCommuns );
-      ( NbrMotsCommuns =:= 0.0,
-        Distance is 1000.0 )
-    ).
+
 /**
  * Prédicat principal.
  * Ce prédicat charge la base de connaissance contenant le texte à résumer.
@@ -35,23 +26,37 @@ resoudre( Nom, K ) :-
 	consult( Nom ),
 	init_teardown(K).
 
+
+
+
 /**
- * ajouterArcs(+Sommet, +ListeSommets)
- * Ajoute des arc/3 entre @Sommet et Chaque membre de @ListeSommets.
- * 
- * @Sommet - Une structure sommet/3
- * @ListeSommets - Une liste de structures sommet/3
+ * Prédicat d'élimination de sommets.
+ * fusionnerSommets(-Sommet)
+ *
+ * Supprime le plus petit arc et tous les arcs connectés aux sommets du plus petit arc.
+ * Modifie la base de donnée avec un nouveau sommet/3 qui est la fusion des deux sommets 
+ * et le retourne.
+ *
+ * @Sommet - le nouveau sommet/3
  */
-ajouterArcs(Sommet, ListeSommets) :-
-	forall(
-		member(Bout, ListeSommets),
-		(
-			sommet(IdA,_, rep(RepA, _)) = Sommet,
-			sommet(IdB,_, rep(RepB, _)) = Bout,
-			distance(RepA, RepB, Distance),
-			assertz(arc(IdA, IdB, Distance))
-		)
-	).
+fusionnerSommets :-
+	aAtteintCible,
+	getArcs(Arcs),
+	trouverMin(Arcs, Min),
+	arc(P1,P2,_) = Min,
+	arc(P2, P1, _) = Bout,
+	retract(sommet(P1, Phrases1, _)),
+	retract(sommet(P2, Phrases2, _)),
+	append(Phrases1, Phrases2, NvPhrases),
+	phraseType(NvPhrases, PhraseType),
+	Sommet = sommet(P1, NvPhrases, PhraseType),
+	!,
+	\+ enleverArcs(Min),
+	assertz(Sommet),
+	actualiserArcs(Sommet),
+	fusionnerSommets.
+	
+
 
 /**
  * phraseType(+ListePhrase, -PhraseType)
@@ -79,31 +84,6 @@ phraseType(ListePhrases, PhraseType) :-
 	PhraseType = rep(PhraseMin, MoyMin).
 
 /**
- * Prédicat d'élimination de sommets.
- * fusionnerSommets(-Sommet)
- *
- * Supprime le plus petit arc et tous les arcs connectés aux sommets du plus petit arc.
- * Modifie la base de donnée avec un nouveau sommet/3 qui est la fusion des deux sommets 
- * et le retourne.
- *
- * @Sommet - le nouveau sommet/3
- */
-fusionnerSommets(Sommet) :-
-	getArcs(Arcs),
-	trouverMin(Arcs, Min),
-	arc(P1,P2,_) = Min,
-	arc(P2, P1, _) = Bout,
-	retract(sommet(P1, Phrases1, _)),
-	retract(sommet(P2, Phrases2, _)),
-	append(Phrases1, Phrases2, NvPhrases),
-	phraseType(NvPhrases, PhraseType),
-	Sommet = sommet(P1, NvPhrases, PhraseType),
-	!,
-	\+ enleverArcs(Min),
-	assertz(Sommet),
-	actualiserArcs(Sommet).
-	
-/**
  * actualiserArcs(+Sommet)
  *
  * Mets à jours la base de connaissances pour inclure les arcs du nouveau @Sommet
@@ -123,28 +103,22 @@ actualiserArcs(Sommet) :-
 	).
 
 /**
- * calculerNombreSommets(-NbrSommets)
- *
- * Retourne le nombre de sommets dans la base de connaissances.
+ * ajouterArcs(+Sommet, +ListeSommets)
+ * Ajoute des arc/3 entre @Sommet et Chaque membre de @ListeSommets.
  * 
- * @NbrSommets - Le nombre de sommets dans la base de connaissances.
+ * @Sommet - Une structure sommet/3
+ * @ListeSommets - Une liste de structures sommet/3
  */
-calculerNombreSommets(NbrSommets) :-
-	getSommets(ListeSommets),
-	length(ListeSommets, NbrSommets).
-
-/**
- * Vérifie si le nombre de sommets cible a été atteint.
- */
-aAtteintCible :-
-	cible(A),
-	compteur(B),
-	B =< A.
-
-/**
- *
- */
-
+ajouterArcs(Sommet, ListeSommets) :-
+	forall(
+		member(Bout, ListeSommets),
+		(
+			sommet(IdA,_, rep(RepA, _)) = Sommet,
+			sommet(IdB,_, rep(RepB, _)) = Bout,
+			distance(RepA, RepB, Distance),
+			assertz(arc(IdA, IdB, Distance))
+		)
+	).
 
 /**
  * enleverArcs(+Arc)
@@ -161,7 +135,26 @@ enleverArcs(Arc) :-
 	retract(arc(Debut, P1, _)),
 	retract(arc(Debut, P2, _)),
 	fail.
-	
+
+
+/**
+ * calculerNombreSommets(-NbrSommets)
+ *
+ * Retourne le nombre de sommets dans la base de connaissances.
+ * 
+ * @NbrSommets - Le nombre de sommets dans la base de connaissances.
+ */
+calculerNombreSommets(NbrSommets) :-
+	getSommets(ListeSommets),
+	length(ListeSommets, NbrSommets).
+
+/**
+ * Vérifie si le nombre de sommets cible a été atteint.
+ */
+aAtteintCible :-
+	cible(A),
+	calculerNombreSommets(B),
+	B > A.
 
 /**
  * trouverMin(+Liste, -Min)
@@ -230,26 +223,60 @@ addition(Phrase, Autre) :-
 	C is A + Distance,
 	asserta(addition(C)).
 
-% -------------- Affichages et getter --------------
 
+/**
+ * Calcul la distance entre deux phrases.
+ * Cette valeur est un point flottant.
+ * distance( +PhraseA, +PhraseB, -Distance ).
+ * les arguments PhraseA et PhraseB sont des listes de mots (atome).
+ * Si les phrases sont trop différente, alors la différence est de 1000.0.
+ */
+distance( PhraseA, PhraseB, Distance ) :-
+    distanceD( PhraseA, PhraseB, DistanceAB ),
+    distanceD( PhraseB, PhraseA, DistanceBA ),
+    Distance is ( DistanceAB + DistanceBA ) / 2.0.
+
+distanceD( PhraseA, PhraseB, Distance ) :-
+    intersection( PhraseA, PhraseB, MotsCommuns ),
+    length( PhraseA, NbrMotA ),
+    length( PhraseB, NbrMotB ),
+    MaxCommun is min( NbrMotA, NbrMotB ),
+    length( MotsCommuns, NbrMotsCommuns ),
+    ( ( \+ NbrMotsCommuns =:= 0.0,
+        Distance is MaxCommun / NbrMotsCommuns );
+      ( NbrMotsCommuns =:= 0.0,
+        Distance is 1000.0 )
+    ).
+
+% -------------- Affichages et getter --------------
 
 /**
  * Affiche les compteurs, les sommet et les arcs.
  */
 affiche :-
-	cible(S), write(S),nl,
-	compteur(C), write(C), nl,
-	getSommets(Sommets),
-	afficher(Sommets),
-	getArcs(Arcs),
-	afficher(Arcs),nl,nl,
-	fusionnerSommets(NvSommet),
-	aAtteintCible,
-	getArcs(NvArcs),
 	getSommets(NvSommets),
-	afficher(NvArcs),
-	afficher(NvSommets).
+	forall(
+		member(Sommet, NvSommets),
+		(
+			sommet(_, _, rep(A,B))=Sommet,
+			concat(A, Phrase),
+			write(B), write(" : "), write(Phrase),write("."),nl
+		)
+	).
 
+/**
+ * concat(+ListeString, -String)
+ *
+ * Concatene une liste de string avec chaque mot séparé d'un espace.
+ *
+ * @ListeString - La liste de string à concatèner.
+ * @String - La chaîne résultante.
+ */
+concat([X,Y|XS], String) :-
+	string_concat(X," ",TmpString),
+	string_concat(TmpString, Y, TmpString2),
+	concat([TmpString2|XS], String).
+concat([X], X).
 
 /**
  * afficher(+Liste)
@@ -292,7 +319,7 @@ getArcs(Arcs) :-
 init_teardown(Cible) :-
 	setup_call_cleanup(
 		initAll(Cible), 
-		affiche,
+		\+ fusionnerSommets,
 		teardownAll
 	).
 
@@ -306,7 +333,6 @@ initAll(Cible) :-
 	asserta(cible(Cible)),
 	initSommets,
 	calculerNombreSommets(NbrSommets),
-	asserta(compteur(NbrSommets)),
 	initArcs.
 
 /**
@@ -334,8 +360,8 @@ initArcs :-
  * Efface tous les prédicats de la base de connaissance.
  */
 teardownAll :-
+	affiche,
 	abolish(cible/1),
-	abolish(compteur/1),
 	abolish(sommet/3),
 	abolish(arc/3).
 
